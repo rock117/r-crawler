fc6149b3b5099c02d12c2af1706c71fdcbdda55b
rock huang <manj22@163.com>
2019-02-21 12:57:06 +0800

    add test code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 mod util;
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+use reqwest as http;
+use scraper::Html;
+use scraper::Selector;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut resp =  http::get("https://www.douban.com/note/704764170/").unwrap();
+    let str = resp.text();
+    // println!("result is: \n{}", str?);
+    let doc = Html::parse_document(&str?);
+    let se = Selector::parse("img").unwrap();
+    let nodes = doc.select(&se);
+    for img in nodes {
+        let src = img.value().attr("src").unwrap();
+        println!("{}", src);
+    }
+    Ok(())
 }
