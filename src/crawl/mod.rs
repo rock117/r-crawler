4f4382c49ea0b2dffc3305244540a0434e177141
rock <manj22@163.com>
2019-04-05 01:16:18 +0800

    implement crawler with multi thread;

--- src/crawl/mod.rs
+++ src/crawl/mod.rs
@@ -4,69 +4,78 @@ use std::collections::HashSet;
 use crate::util::url_parser;
 use reqwest as http;
 use crate::util::url_parser::parse;
+use std::sync::{Arc, Mutex};
+use reqwest::Response;
+use threadpool::ThreadPool;
+
+use std::thread;
+use std::sync::mpsc::{sync_channel, SyncSender, Receiver};
+use std::io::Read;
 
 pub struct Crawler {
     entry: String,
     started: bool,
     crawled_urls: HashSet<String>,
-    current_num: u32
+    current_num: i32,
+    pool: ThreadPool
 }
 
 impl Crawler {
 
     pub fn new(entry: String) -> Self {
-        Crawler { entry, started: false, crawled_urls: HashSet::new(), current_num: 0}
+        Crawler { entry, started: false, crawled_urls: HashSet::new(), current_num: 0, pool: ThreadPool::new(50)}
     }
 
     pub fn start(&mut self) {
         if self.started {
             return;
         }
-        self.started = true;
         let url = self.entry.clone();
-        self.crawl(url.as_ref());
+        let (tx, rx) = sync_channel(10);
+
+        self.init(url, &tx);
+        self.loop2(tx, rx)
+    }
+
+    fn init(&mut self, url: String, tx: &SyncSender<(String, Response)>) {
+        let t = tx.clone();
+        let n = self.current_num;
+        self.pool.execute(move || {
+            crawl(n, url, t);
+        });
+    }
+
+    fn loop2(&mut self, tx: SyncSender<(String, Response)>, rx: Receiver<(String, Response)>) -> () {
+        while let Ok((url, mut resp)) = rx.recv() {
+            let urls = self.handle_page(url.as_ref(), read_content(&mut resp).as_ref());
+            for url in urls {
+                let tx = tx.clone();
+                self.current_num = self.current_num + 1;
+                let n = self.current_num;
+                self.pool.execute(move || {
+                    crawl(n, url, tx);
+                });
+            }
+        }
     }
 
     fn is_done(&self) -> bool {
         self.current_num > 1009
     }
 
-    fn crawl(&mut self, url: &str) {
-        self.current_num = self.current_num + 1;
 
-        println!("{} Begin to fetch {}", self.current_num, url);
-        let resp = http::get(url);
-        if resp.is_err() {
-            return;
-        }
-        let html = resp.unwrap().text();
-        if html.is_err() {
-            return;
-        }
-        let html = html.unwrap();
-        self.handle_page(url, html.as_str());
-    }
 
     fn has_crawled(&self, url: &str) -> bool {
         self.crawled_urls.contains(url)
     }
 
-    fn handle_page(&mut self, parent_url: &str, html: &str) {
-        if self.is_done() {
-            eprintln!("Crawl done!");
-            return;
-        }
+    fn handle_page(&mut self, parent_url: &str, html: &str) -> Vec<String> {
         let links: Vec<String> = url_parser::parse(html);
         let links = links.iter()
             .map(|url| get_real_url(url, parent_url))
             .filter(|url| !self.has_crawled(url.as_ref()) && url.contains("oschina.net"))
             .collect::<Vec<String>>();
-
-
-        for link in links {
-            self.crawled_urls.insert(link.to_string());
-            self.crawl(link.as_ref());
-        }
+        links
     }
 
 
@@ -90,9 +99,26 @@ fn parse_url(url: &str) -> Option<(&str, &str, &str)> {
         .split("/").collect();
     let host = url_parts[0];
     let protocol: &str = url.split("://").collect::<Vec<&str>>()[0];
-    let path: &str = match url.trim_left_matches(format!("{}://{}", protocol, host).as_ref() as &str) {
+    let path: &str = match url.trim_start_matches(format!("{}://{}", protocol, host).as_ref() as &str) {
         "" => "/",
         p => p
     };
     Some((protocol, host, path))
+}
+
+fn read_content(resp: &mut Response) -> String {
+    let mut buf = String::new();
+    resp.read_to_string(&mut buf);
+    buf
+}
+
+
+fn crawl(curr: i32, url: String,   tx: SyncSender<(String, Response)>){
+    println!("Begin to crawl {} {}", curr, &url);
+    let resp = http::get(url.as_ref() as &str);
+    if resp.is_err() {
+        return;
+    }
+    let mut resp = resp.unwrap();
+    tx.send((url, resp));
 }
--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,13 @@ mod html;
 mod crawl;
 
 use crawl::Crawler;
-//use threadpool::ThreadPool;
-//use std::time::Duration;
-//use std::cell::Cell;
+use std::rc::Rc;
+use std::cell::{Cell, RefCell};
+use libc;
+use std::io::Write;
+use std::fmt::Debug;
 
 fn main() {
     let mut crawl: Crawler = Crawler::new("https://www.oschina.net/".to_owned());
     crawl.start();
 }
-
-struct M {
-    name: String
-}
-
-impl M {
-    fn init(&mut self) {
-        println!("init running...");
-        let n = self.name.clone();
-        let n = n.as_ref();
-        self.hello(n);
-    }
-    fn hello(&mut self, name: &str) {
-        println!("hello {}", name);
-    }
-}
-
-
-//println!("hello world");
-// let mut crawl: Crawler = Crawler::new("https://www.oschina.net/".to_owned());
-// crawl.start();
-
-
-
-
-
-
-
-
-
-
-
